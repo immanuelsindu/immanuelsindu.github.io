71e66a7e21d9af3a65f8df64fcdd140be379a26e
immanuelsindu <immanuel.sindu@ti.ukdw.ac.id>
2023-07-11 18:45:01 +0700

    update lagi

--- deploy.sh
+++ deploy.sh
@@ -12,12 +12,10 @@ cd dist
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
 
+
 git init
 git add -A
 git commit -m 'myDeploy'
-
-git config --global user.email "immanuel.sindu@ti.ukdw.ac.id"
-git config --global user.name "immanuelsindu"
 
 # if you are deploying to https://<USERNAME>.github.io
 git push -f git@github.com:immanuelsindu/immanuelsindu.github.io.git master
