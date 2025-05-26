a8d9b0e04fd55af90c0155038b1a6da80b6af3b8
immanuelsindu <immanuelsindu@ti.ukdw.ac.id>
2025-05-26 18:04:30 +0700

    chore : update config

--- svelte.config.js
+++ svelte.config.js
@@ -1,22 +1,12 @@
-// import adapter from "@sveltejs/adapter-auto";
+import adapter from "@sveltejs/adapter-auto";
 // import adapter from "@sveltejs/adapter-node";
 // svelte.config.js
-import adapter from '@sveltejs/adapter-static';
+// import adapter from '@sveltejs/adapter-static';
 const dev = process.env.NODE_ENV !== 'production';
 const repoName = 'immanuelsindu.github.io'; // Ganti dengan nama repo kamu
 
 export default {
   kit: {
-    adapter: adapter({
-      pages: 'build',
-      assets: 'build',
-      fallback: 'index.html'
-    }),
-    paths: {
-      base: dev ? '' : `/${repoName}`
-    },
-    prerender: {
-      entries: [] // agar tidak error dynamic routes
-    }
+    adapter: adapter(),
   }
 };
